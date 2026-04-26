d24c5b8c5178a86736bef3182c98772aae620760
shulesoft8 <shulesoft8@gmail.com>
2026-04-26 20:26:00 +0800

    fix: export ShieldIcon from Icons.jsx to resolve build error

--- src/components/Common/Icons.jsx
+++ src/components/Common/Icons.jsx
@@ -108,6 +108,8 @@ export const SecurityIcon = (p) => (
   </Ico>
 );
 
+export const ShieldIcon = SecurityIcon;
+
 export const SubscriptionsIcon = (p) => (
   <Ico {...p}>
     <path d="M21 16V8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v8a2 2 0 0 0 1 1.73l7 4a2 2 0 0 0 2 0l7-4A2 2 0 0 0 21 16z"/>
